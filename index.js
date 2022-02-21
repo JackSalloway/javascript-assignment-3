ca310733b6fab0016417fd6f9f061e11092ada29
Jack Salloway <jacksalloway@hotmail.com>
2022-02-21 12:33:08 +0000

    Add operations object
    Move operator functions in to operations object
    
    Add event listener for equals button

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let calcArr = [];
 let calcInput = '';
+const operations = {
+    add: (a, b) => {
+        return a + b;
+    },
+    subtract: (a, b) => {
+        return a - b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    }
+}
 
 // query selectors
+// button query selectors
 const buttons = document.querySelectorAll('.button');
 const buttonOperators = document.querySelectorAll('.button-operator');
 const buttonEquals = document.getElementById('equals');
 
 // event listeners
+// select numbers for equation
 buttons.forEach(button => {
 
     button.addEventListener('click', (e) => {
@@ -14,33 +30,24 @@ buttons.forEach(button => {
     });
 });
 
+// select operator for equation
 buttonOperators.forEach(button => {
     button.addEventListener('click', (e) => {
         if (calcArr.length === 2) {
             return;
         }
-        calcArr.push(parseInt(calcInput));
         calcArr.push(e.target.id);
+        calcArr.push(parseInt(calcInput));
         return calcInput = '';
     });
 });
 
-const add = (a, b) => {
-    return a + b;
-};
-
-const subtract = (a, b) => {
-    return a - b;
-};
-
-const multiply = (a, b) => {
-    return a * b;
-};
-
-const divide = (a, b) => {
-    return a / b;
-};
+// select final number for equation and equate
+buttonEquals.addEventListener('click', () => {
+    calcArr.push(parseInt(calcInput));
+    console.log(operate(calcArr[0], calcArr[1], calcArr[2]));
+});
 
 const operate = (operator, a, b) => {
-    return operator(a, b);
+    return operations[operator](a, b);
 };
